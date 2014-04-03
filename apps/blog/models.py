5f5cb787bd3034b1a0e011850cbdf3f610ee9101
Woile <santiwilly@gmail.com>
2014-04-02 22:28:59 -0300

    blabla

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class Post(models.Model):
-    author = models.ForeignKey(User, blank=True, null=True)
+    author = models.ForeignKey(User, null=True)
     title = models.CharField(blank=False, max_length=100)
     content = models.TextField(blank=False, max_length=40)
     datetime = models.DateTimeField(blank=True, auto_now_add=True)
@@ -18,7 +19,30 @@ class Post(models.Model):
 class Comment(models.Model):
     author = models.ForeignKey(User)
     post = models.ForeignKey(Post)
-    content = models.CharField(blank=False, max_length=100)
+    content = models.TextField(blank=False, max_length=100)
 
+
+class PerfilUser(models.Model):
+    user = models.OneToOneField(User)
+    GENRE_CHOICES = (
+        ('m', 'Masculino'),
+        ('f', 'Femenino'),
+    )
+    AUTHORIZATION = (
+        ('Y', 'YES'),
+        ('N', 'NO'),
+    )
+    genre = models.CharField(max_length=1, choices=GENRE_CHOICES, null=True)
+    birth_date = models.DateField(null=True)
+    create_post = models.CharField(max_length=1, choices=AUTHORIZATION, null=True)
+    comment = models.CharField(max_length=1, choices=AUTHORIZATION, null=True)
+
+## Used ONLY when creted an User, this will create an empty PerfilUser
+#def create_user_profile(sender, instance, created, **kwargs):
+    #if created:
+        #PerfilUser.objects.create(user=instance)
+
+## This signal is sent after the save is done, so, after a User is created
+#post_save.connect(create_user_profile, sender=User)
 
 
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import logging
 
 from django.shortcuts import render, redirect, get_object_or_404
@@ -5,18 +6,50 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, Http404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-
-from django.contrib.auth.views import login
+from django.contrib.auth.forms import UserCreationForm  # Form to create user
+#from django.contrib.auth.views import login
 from apps.blog.models import Post
 from apps.blog.forms import PostForm
+from apps.blog.forms import CommentForm, UserRegisterForm, PerfilUserForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+#def home(request):
+    #posts = Post.objects.all().order_by('-datetime')[:2]
+    #return render(request, "blog/home.html", {'posts': posts})
+
 
 def home(request):
-    posts = Post.objects.all()[:2]
-    return render(request, "blog/home.html", {'posts': posts})
+    """Paginated 2 posts order by -datetime"""
+    post_list = Post.objects.all().order_by('-datetime')
+    paginator = Paginator(post_list, 2)  # 2 per page (for testing purpouses)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)  # delivers first page
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/home.html', {'posts': posts})
+
+
+def create_user(request):
+    """ Under development """
+    form = UserRegisterForm(request.POST or None)
+    form2 = PerfilUserForm(request.POST or None)
+    if form.is_valid() and form2.is_valid():
+        psw = form.clean_password2()  # executes def inside UserRegisterForm
+        new_user = form.save(commit=False)
+        new_user.set_password(psw)
+        new_user.save()
+        profile = form2.save(commit=False)
+        profile.user = new_user
+        profile.save()
+        return redirect(reverse('login'))
+    return render(request, 'registration.html', {'form': form, 'form2': form2})
 
 
 def posts(request):
-
     posts = Post.objects.all().order_by("-datetime")
     result = {}
     for p in posts:
@@ -24,16 +57,33 @@ def posts(request):
     #posts = Post.objects.filter(title__startswith="C")
     return render(request, "blog/posts.html", {'posts': result})
 
+
+def view_post(request, post_id):
+    """View a single post, commenting DOES NOT requiere validation from author
+    >>Must do: Author CAN remove comment"""
+    post_instance = get_object_or_404(Post, id=post_id)
+    form = CommentForm(request.POST or None)
+    if form.is_valid():
+        user = request.user
+        comment = form.save(commit=False)
+        comment.author = user
+        comment.post = post_instance
+        comment.save()
+        return redirect(reverse("view_post", kwargs={'post_id': post_id}),
+            {'post': post_instance, 'form': form})
+    return render(request, "blog/view_post.html",
+        {'post': post_instance, 'form': form})
+
+
 @login_required
 def create_post(request):
-
-    form = PostForm(request.POST or None)
-    if form.is_valid(): # All validation rules pass
+    form = PostForm(request.POST or None)  # Por qué or None?
+    if form.is_valid():  # All validation rules pass
         user = request.user
         post = form.save(commit=False)
         post.author = user
         post.save()
-        return redirect(reverse("blog_posts"))
+        return redirect(reverse("blog_home"))
 
     return render(request, "blog/create_post.html", {'form': form})
 
@@ -42,11 +92,15 @@ def edit_post(request, post_id):
     logging.critical(request.method)
     a_post_instance = get_object_or_404(Post, id=post_id)
     form = PostForm(request.POST or None, instance=a_post_instance)
-    if form.is_valid(): # All validation rules pass
-        post = form.save()
+    if form.is_valid():  # All validation rules pass
+        form.save()
         return redirect(reverse("blog_posts"))
 
     return render(request, "blog/edit_post.html", {'form': form})
+
+
+def view_profile(request):
+    return render(request, 'view_profile.html')
 
 
 # ---------------------------------------------------
@@ -54,26 +108,20 @@ def edit_post(request, post_id):
 # ---------------------------------------------------
 
 
-def create_post_first_aproach(request):
+#def create_post_first_aproach(request):
 
-    if request.method == "POST":
-        form = PostForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
-            # Process the data in form.cleaned_data
-            # ...
-            user = User.objects.all()[0]
-            post = form.save(commit=False)
-            post.author = user
-            post.save()
-            return redirect(reverse("blog_posts")) # Redirect after POST
-    else:
-        form = PostForm()
+    #if request.method == "POST":
+        #form = PostForm(request.POST)  # A form bound to the POST data
+        #if form.is_valid():  # All validation rules pass
+            ## Process the data in form.cleaned_data
+            ## ...
+            #user = User.objects.all()[0]
+            #post = form.save(commit=False)
+            #post.author = user
+            #post.save()
+            #return redirect(reverse("blog_posts"))  # Redirect after POST
+    #else:
+        #form = PostForm()
 
-    return render(request, "blog/create_post.html", {'form': form})
-
-
-def comment_post(request):
-    form = None
-    return render(request, "blog/comment_post.html", {'form': form})
-
+    #return render(request, "blog/create_post.html", {'form': form})
 
